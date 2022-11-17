e8f0146bbf5fcfefd0a918d6c12033205adedf2f
ig.rudenko <ig.rudenko@users.noreply.github.com>
2022-11-17 21:56:11 +0300

    v 0.5.2 bugfix

--- deco.py
+++ deco.py
@@ -14,6 +14,8 @@ def timer(function):
 
         end: float = time.time()
 
+        print(f"Время выполнения функции {function.__name__} -> {end - start} сек")
+
         return result
 
     print("Передали в функцию wrapper", function.__name__)
